47ff04c4807c17f855de4b1db2d77e441336d54e
Nitin Srivastava <nitin07.gkp@gmail.com>
2016-12-04 23:17:11 +0530

    Robot initialized with commands

--- lib/robot.rb
+++ lib/robot.rb
@@ -1,2 +1,12 @@
 class Robot
+
+  VALID_COMMANDS = ['PLACE', 'LEFT','RIGHT','MOVE','REPORT']
+
+  attr_accessor :commands, :table
+
+  def initialize(commands, stderr=STDERR)
+    @commands = commands
+    @error = stderr
+  end
+
 end
--- spec/robot_spec.rb
+++ spec/robot_spec.rb
@@ -3,9 +3,19 @@ require_relative '../lib/robot'
 
 describe Robot do
 
-  let(:robot) { Robot.new() }
+  let(:commands) { ['PLACE 1,2,NORTH', 'RIGHT', 'MOVE', 'MOVE', 'REPORT']}
+  let(:robot) { Robot.new(commands) }
 
   describe "#new" do
-    it { is_expected.to be_a(Robot) }
+    context 'When robot is initialized with commands' do
+      it 'should be a valid instance of robot' do
+        expect(robot).to be_instance_of Robot
+      end
+
+      it 'should have commands' do
+        expect(robot.commands).to match_array(commands)
+      end
+    end
+
   end
 end
