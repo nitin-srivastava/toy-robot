ab98e6686f4da8c5e4998808dab6d4a95929ff45
Nitin Srivastava <nitin07.gkp@gmail.com>
2016-12-04 21:12:13 +0530

    Table X, Y coordinates and directions

--- lib/table.rb
+++ lib/table.rb
@@ -1,5 +1,11 @@
 class Table
 
+  attr_accessor :xaxis, :yaxis, :directions
 
+  def initialize(x=5, y=5)
+    @xaxis = (0..x.to_i)
+    @yaxis = (0..y.to_i)
+    @directions = %w(NORTH EAST SOUTH WEST)
+  end
 
 end
--- spec/table_spec.rb
+++ spec/table_spec.rb
@@ -8,6 +8,26 @@ describe Table do
 
     context 'When 5X5 table is created' do
       it {is_expected.to be_a(Table)}
+
+      it 'should have 0 minimum x coordinate' do
+        expect(table.xaxis.min).to be 0
+      end
+
+      it 'should have 5 maximum x coordinate' do
+        expect(table.xaxis.max).to be 5
+      end
+
+      it 'should have 0 minimum y coordinate' do
+        expect(table.yaxis.min).to be 0
+      end
+
+      it 'should have 5 maximum y coordinate' do
+        expect(table.yaxis.max).to be 5
+      end
+
+      it 'should have NORTH, EAST, SOUTH and WEST directions' do
+        expect(table.directions).to eq ['NORTH', 'EAST', 'SOUTH', 'WEST']
+      end
     end
   end
 
