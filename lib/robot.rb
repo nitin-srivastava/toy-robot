94f4aa6d723c0c57615ae46fa6d7d96819c2ab71
Nitin Srivastava <nitin07.gkp@gmail.com>
2016-12-06 16:41:03 +0530

    Robot placed command

--- lib/robot.rb
+++ lib/robot.rb
@@ -4,12 +4,20 @@ class Robot
 
   VALID_COMMANDS = ['PLACE', 'LEFT','RIGHT','MOVE','REPORT']
 
-  attr_accessor :commands, :table
+  attr_accessor :commands, :table, :x_coordinate, :y_coordinate, :orientation
 
   def initialize(commands, stderr=STDERR)
     @commands = commands
     @error = stderr
     @table = Table.new()
+  end
+
+  # place robot on table
+  def place(args)
+    x, y, orientation = args
+    self.x_coordinate = x.to_i
+    self.y_coordinate = y.to_i
+    self.orientation = orientation
   end
 
 end
--- spec/robot_spec.rb
+++ spec/robot_spec.rb
@@ -7,8 +7,8 @@ describe Robot do
   let(:commands) { ['PLACE 1,2,NORTH', 'RIGHT', 'MOVE', 'MOVE', 'REPORT']}
   let(:robot) { Robot.new(commands) }
 
-  describe "#new" do
-    context 'When robot is initialized with commands' do
+  describe '#new' do
+    context 'When robot is initialized' do
       it 'should be a valid instance of robot' do
         expect(robot).to be_instance_of Robot
       end
@@ -21,6 +21,26 @@ describe Robot do
         expect(robot.table).to be_instance_of(Table)
       end
     end
+  end
+
+  describe '#place' do
+
+    context 'When robot is placed on the table' do
+      before { robot.place([1, 2, 'NORTH'])}
+
+      it 'should assign 1 to robot x coordinate' do
+        expect(robot.x_coordinate).to eq 1
+      end
+
+      it 'should assign 2 to robot y coordinate' do
+        expect(robot.y_coordinate).to eq 2
+      end
+
+      it 'should set robot orientation to NORTH' do
+        expect(robot.orientation).to eq 'NORTH'
+      end
+    end
 
   end
+
 end
